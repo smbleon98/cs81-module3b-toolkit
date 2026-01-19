4abd42acb8a2dd92843f4a220dee03a633343dd2
Shadia Milon <milon_shadia_nasri01@student.smc.edu>
2026-01-18 17:28:52 -0800

    Adding square() function

--- math.js
+++ math.js
@@ -3,8 +3,21 @@ function double(x){
     return (x*2);
 }
 
+/*
 // Tests for Function 1
 console.log(double(0));
 console.log(double(3));
 console.log(double(-2));
 console.log(double(-5));
+*/
+
+// Function 2: Square
+function square(x){
+    return (x*x);
+}
+
+// Tests for Function 2
+console.log(square(0));     // 0
+console.log(square (3));     // 6
+console.log(square(-2));    // -4
+console.log(square(-5));     
